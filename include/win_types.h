12bed7fc33ad309b4ad57aa20e87420491eb06f2
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-01 15:09:12 +0800

    win_types.h: add header file for define typedef for Windows type.

---
+++ include/win_types.h
@@ -0,0 +1,48 @@
+#ifndef WIN_TYPES_H
+#define WIN_TYPES_H
+
+#include "c_types.h"
+
+typedef int32 BOOL;
+typedef uint8 BOOLEAN;
+
+typedef uint8  BYTE;
+typedef uint16 WORD;
+typedef uint32 DWORD;
+typedef uint64 QWORD;
+
+typedef int8   CHAR;
+typedef int16  SHORT;
+typedef int32  LONG;
+typedef int64  LONGLONG;
+typedef uint8  UCHAR;
+typedef uint16 USHORT;
+typedef uint32 ULONG;
+typedef uint64 ULONGLONG;
+
+typedef uint UINT;
+typedef uint SIZE_T;
+typedef uint ULONG_PTR;
+
+typedef void* POINTER;
+typedef void* HANDLE;
+
+typedef void*   PVOID;
+typedef uint8*  PSTR;
+typedef uint16* PWSTR;
+typedef HANDLE* PHANDLE;
+
+typedef void*   LPVOID;
+typedef uint8*  LPSTR;
+typedef uint16* LPWSTR;
+typedef HANDLE* LPHANDLE;
+
+typedef const void*   PCVOID;
+typedef const uint8*  PCSTR;
+typedef const uint16* PCWSTR;
+
+typedef const void*   LPCVOID;
+typedef const uint8*  LPCSTR;
+typedef const uint16* LPCWSTR;
+
+#endif // WIN_TYPES_H
